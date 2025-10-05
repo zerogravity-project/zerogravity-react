b883c17506688b2e7f3a6e7f5cdbd8f38ebb752a
minukHwang <damin2003@yonsei.ac.kr>
2025-10-05 16:26:36 +0900

    :sparkles: feat: implement 3D plant interaction with floating button and responsive clock

--- src/app/_components/ui/navigation/MenuNavigation.tsx
+++ src/app/_components/ui/navigation/MenuNavigation.tsx
@@ -9,21 +9,22 @@ import { Link as RadixLink } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 
 import Icon from '@/app/_components/ui/icon/Icon';
+import { useDateTime } from '@/app/_hooks/useDateTime';
 import { Color } from '@/app/style/type';
 import { cn } from '@/lib/utils';
 
 import { RadixButton } from '../button';
 import { LogoSvg } from '../others';
 
-interface MenuItem {
-  label: string;
-  href: string;
-}
+// interface MenuItem {
+//   label: string;
+//   href: string;
+// }
 
-const MENU_ITEMS: MenuItem[] = [
-  { label: 'Home', href: '/' },
-  { label: 'About', href: '/about' },
-];
+// const MENU_ITEMS: MenuItem[] = [
+//   { label: 'Home', href: '/' },
+//   { label: 'About', href: '/about' },
+// ];
 
 interface TopNavigationProps {
   background?: boolean;
@@ -31,6 +32,7 @@ interface TopNavigationProps {
 
 export default function TopNavigation({ background }: TopNavigationProps) {
   const { data: session, status } = useSession();
+  const { dateData } = useDateTime();
 
   const isAuthenticated = status === 'authenticated';
   const profileImage = session?.user?.image ?? null;
@@ -39,7 +41,7 @@ export default function TopNavigation({ background }: TopNavigationProps) {
   return (
     <NavigationMenu.Root
       className={cn(
-        'h-topnav-height relative z-10 flex w-full flex-1 items-center justify-between px-6',
+        'h-topnav-height relative z-10 flex w-full flex-1 items-center justify-between px-6 sm:px-8',
         background && 'border-b border-[var(--gray-3)] bg-white'
       )}
     >
@@ -52,33 +54,43 @@ export default function TopNavigation({ background }: TopNavigationProps) {
         </RadixLink>
 
         {/* Menu */}
-        <NavigationMenu.List className="hidden sm:flex sm:items-center">
+        {/* <NavigationMenu.List className="hidden sm:flex sm:items-center">
           {MENU_ITEMS.map(item => (
             <NavigationMenu.Item key={item.href}>
               <MenuLink href={item.href} label={item.label} color="gray" />
             </NavigationMenu.Item>
           ))}
-        </NavigationMenu.List>
+        </NavigationMenu.List> */}
       </div>
 
-      {/* Profile */}
-      <div className="hidden sm:flex">
-        {isAuthenticated && (
-          <NavigationMenu.Link href="/profile">
-            <Avatar.Root className="inline-flex h-8 w-8 items-center justify-center overflow-hidden rounded-full">
-              {profileImage && <Image src={profileImage} alt="profile" width={32} height={32} />}
-              {!profileImage && <Avatar.Fallback>{displayName.charAt(0)}</Avatar.Fallback>}
-            </Avatar.Root>
-          </NavigationMenu.Link>
-        )}
+      {/* 날짜 표시 */}
+      <div className="flex items-center gap-16">
+        <div className="hidden h-8 items-center justify-center overflow-hidden sm:flex">
+          <span className="text-sm leading-[0.9] text-[var(--gray-a10)]">
+            {dateData ? dateData.year : '0000'}年 · {dateData ? dateData.month : '00'}月 ·{' '}
+            {dateData ? dateData.day : '00'}日 · {dateData ? dateData.weekday : ''}
+          </span>
+        </div>
 
-        {!isAuthenticated && <MenuLink href="/login" label="Login" />}
+        {/* Profile */}
+        <div className="hidden sm:flex">
+          {isAuthenticated && (
+            <NavigationMenu.Link href="/profile">
+              <Avatar.Root className="inline-flex h-8 w-8 items-center justify-center overflow-hidden rounded-full">
+                {profileImage && <Image src={profileImage} alt="profile" width={32} height={32} />}
+                {!profileImage && <Avatar.Fallback>{displayName.charAt(0)}</Avatar.Fallback>}
+              </Avatar.Root>
+            </NavigationMenu.Link>
+          )}
+
+          {!isAuthenticated && <MenuLink href="/login" label="Login" />}
+        </div>
+
+        {/* Hamburger Menu */}
+        <RadixButton size="2" variant="outline" radius="small" color="gray" className="!pr-1 !pl-1 sm:!hidden">
+          <Icon color="tomato">menu</Icon>
+        </RadixButton>
       </div>
-
-      {/* Hamburger Menu */}
-      <RadixButton size="2" variant="outline" radius="small" color="gray" className="!pr-1 !pl-1 sm:!hidden">
-        <Icon color="tomato">menu</Icon>
-      </RadixButton>
     </NavigationMenu.Root>
   );
 }
--- src/app/_components/ui/scene/PlantModel.tsx
+++ src/app/_components/ui/scene/PlantModel.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { useCursor, useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import { useControls } from 'leva';
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 
-import { useHoverState } from './HoverContext';
+import { useMouseEventState } from './MouseEventContext';
 
 interface PlantModelProps {
   onLoad?: () => void;
@@ -14,10 +17,11 @@ interface PlantModelProps {
 
 // GLTF 모델 컴포넌트
 export const PlantModel = forwardRef<THREE.Group, PlantModelProps>(({}, ref) => {
+  const router = useRouter();
   // 모델 위치, 회전 설정 GUI
   const { positionX, positionY, positionZ, rotationX, rotationY, rotationZ } = useControls('PlantModel', {
     positionX: { value: 0, min: -10, max: 10 },
-    positionY: { value: -0.15, min: -10, max: 10 },
+    positionY: { value: -0.08, min: -10, max: 10, step: 0.01 },
     positionZ: { value: 0, min: -10, max: 10 },
     rotationX: { value: 0.2, min: -Math.PI, max: Math.PI },
     rotationY: { value: 1.7, min: -Math.PI, max: Math.PI },
@@ -25,15 +29,68 @@ export const PlantModel = forwardRef<THREE.Group, PlantModelProps>(({}, ref) => 
   });
 
   // GLTF 모델 로드
-  const { scene } = useGLTF('/models/potted-plant-04/potted_plant_04_4k.gltf');
+  const { scene } = useGLTF('/models/potted-plant/potted_plant_04_4k.gltf');
 
   // Context에서 상태 업데이트 함수 가져오기
-  const { hovered, setHovered, setDomCoords } = useHoverState();
+  const {
+    hovered,
+    clickedForThreeSeconds,
+    setHovered,
+    setClicked,
+    setDomCoords,
+    setClickedForThreeSeconds,
+    setProgress,
+  } = useMouseEventState();
   // 3D 모델과 마우스 호버 상태 연동
   useCursor(hovered);
 
+  // 애니메이션을 위한 상태
+  const [targetY, setTargetY] = useState(positionY);
+  const [currentY, setCurrentY] = useState(positionY);
+  const [targetRotationY, setTargetRotationY] = useState(rotationY);
+  const [currentRotationY, setCurrentRotationY] = useState(rotationY);
+
+  // 클릭 상태에 따른 목표 Y 위치 설정
+  useEffect(() => {
+    if (clickedForThreeSeconds) {
+      setHovered(false);
+      setTimeout(() => {
+        router.push('/spaceout/main');
+        // 3초 이상 클릭되면 높이 올리기
+        // setTargetY(positionY + 0.5);
+        // setTargetRotationY(rotationY + Math.PI * 2);
+      }, 1000);
+    } else {
+      // 일반 클릭이나 클릭 해제시 원래 위치
+      setTargetY(positionY);
+      setTargetRotationY(rotationY);
+    }
+  }, [setHovered, positionY, rotationY, clickedForThreeSeconds, router]);
+
+  // 부드러운 애니메이션
+  useFrame((_, delta) => {
+    const lerpFactor = 0.5 * delta; // 애니메이션 속도 조절
+    const newY = THREE.MathUtils.lerp(currentY, targetY, lerpFactor);
+    const newRotationY = THREE.MathUtils.lerp(currentRotationY, targetRotationY, lerpFactor);
+    setCurrentY(newY);
+    setCurrentRotationY(newRotationY);
+
+    // 진행률 계산 (클릭 중일 때만)
+    if (pointerDownTimeRef.current) {
+      const elapsed = Date.now() - pointerDownTimeRef.current;
+      const progressPercent = Math.min(elapsed / 3000, 1); // 3초 = 100%
+      setProgress(progressPercent);
+
+      // 3초 도달 시
+      if (progressPercent >= 1 && !clickedForThreeSeconds) {
+        setClickedForThreeSeconds(true);
+      }
+    }
+  });
+
   // 타이머 ID를 저장할 ref
   const timeoutRef = useRef<number | null>(null);
+  const pointerDownTimeRef = useRef<number | null>(null);
 
   /**
    * 마우스 이벤트 핸들러
@@ -68,6 +125,24 @@ export const PlantModel = forwardRef<THREE.Group, PlantModelProps>(({}, ref) => 
     event.stopPropagation();
   };
 
+  const onPointerDown = (e: any) => {
+    e.stopPropagation();
+    setClicked(true);
+
+    // 클릭 시작 시간 기록
+    pointerDownTimeRef.current = Date.now();
+    setProgress(0);
+  };
+
+  const onPointerUp = (e: any) => {
+    e.stopPropagation();
+    setClicked(false);
+
+    // 클릭 시간 리셋
+    pointerDownTimeRef.current = null;
+    setProgress(0);
+  };
+
   useEffect(() => {
     if (hovered) {
       document.body.style.cursor = 'none';
@@ -80,11 +155,15 @@ export const PlantModel = forwardRef<THREE.Group, PlantModelProps>(({}, ref) => 
     <primitive
       ref={ref}
       object={scene}
-      position={[positionX, positionY, positionZ]}
-      rotation={[rotationX, rotationY, rotationZ]}
+      position={[positionX, currentY, positionZ]}
+      rotation={[rotationX, currentRotationY, rotationZ]}
       onPointerOver={onPointerOver}
       onPointerMove={onPointerMove}
       onPointerOut={onPointerOut}
+      onPointerDown={onPointerDown}
+      onPointerUp={onPointerUp}
+      receiveShadow
+      castShadow
     />
   );
 });
